72541830730a78c261fd06aec62f41855162fa3d
nagisa-win <yjz.dev@outlook.com>
2025-06-11 18:59:14 +0800

    Go online

---
+++ README.md
@@ -0,0 +1,21 @@
+# Web Router
+
+# 简单网站路由
+
+> Inspired by http://v50to.me
+
+## 基本逻辑
+
+- 如果是周末，则只会跳转[开通淘宝88VIP页面](https://pages-fast.m.taobao.com/wow/z/blackvip/v/pc-super)和[京东PLUS会员页面](https://plus.jd.com/index)
+
+- 如果是周四，会跳转[肯德基官网](https://www.kfc.com.cn/)
+- 如果是周五，会跳转[麦当劳官网](https://www.mcdonalds.com.cn/)
+- 对了，如果是工作日，有5%概率跳转[华莱士官网](https://www.cnhls.com/)
+
+## 部署
+
+- Github Page
+
+## [v88to.me](https://v88to.me)
+
+
---
+++ static/index.js
@@ -0,0 +1,107 @@
+var router = (function (window) {
+    var router = {};
+
+    router.redirect = function (path) {
+        if (window.$stay) {
+            console.log('Stayed', path);
+            return;
+        }
+        console.log('Redirect', path);
+        setTimeout(() => {
+            window.location.href = path;
+        }, 1000);
+    };
+    router.roll = function (...arr) {
+        if (!Array.isArray(arr) || arr.length === 0) {
+            return '';
+        }
+        var randomIndex = Math.floor(Math.random() * arr.length);
+        return arr[randomIndex];
+    };
+
+    function _isMobile() {
+        var ua = window.navigator.userAgent || window.navigator.vendor || window.opera;
+        return !!(/android/i.test(ua) || /iPhone|iPad|iPod/i.test(ua) || /mobile/i.test(ua));
+    }
+
+    router._random = Math.random();
+    router._rand = Math.random;
+    router._date = new Date();
+    router._now = Date.now();
+    router._year = router._date.getFullYear();
+    router._month = router._date.getMonth() + 1;
+    router._day = router._date.getDate();
+    router._week = router._date.getDay();
+    router._hour = router._date.getHours();
+    router._minute = router._date.getMinutes();
+    router._second = router._date.getSeconds();
+    router._isMobile = _isMobile();
+
+    router.Map = {
+        vip: [
+            {
+                mobile: 'https://pages-fast.m.taobao.com/wow/z/blackvip/v/super-c',
+                pc: 'https://pages-fast.m.taobao.com/wow/z/blackvip/v/pc-super',
+            },
+            {
+                mobile: 'https://plus.jd.com/index',
+                pc: 'https://plus.jd.com/index',
+            },
+        ],
+        food: [
+            {
+                mobile: 'https://www.cnhls.com/',
+                pc: 'https://www.cnhls.com/',
+            },
+            {
+                mobile: 'https://m.kfc.com.cn/',
+                pc: 'https://www.kfc.com.cn/',
+            },
+            {
+                mobile: 'https://www.mcdonalds.com.cn/',
+                pc: 'https://www.mcdonalds.com.cn/',
+            },
+            {
+                mobile: 'https://bkchina.cn/mobile/main.html',
+                pc: 'https://www.bkchina.cn/',
+            },
+            {
+                mobile: 'http://weixin.qq.com/r/xTpVTY3ERtA5rcku92-X',
+                pc: 'http://www.saizeriya.com.cn/',
+            },
+            {
+                mobile: 'https://www.pizzahut.com.cn/',
+                pc: 'https://www.pizzahut.com.cn/',
+            },
+        ],
+    };
+
+    return router;
+})(window);
+
+function doRedirect() {
+    var key = router._isMobile ? 'mobile' : 'pc';
+    var category, location;
+    if (router._week === 6 || router._week === 0) {
+        category = 'vip';
+    } else {
+        category = router.roll('food', 'vip');
+    }
+
+    if (category === 'food') {
+        if (router._random < 0.1) {
+            location = router.Map[category][0][key];
+        } else if (router._week === 4) {
+            location = router.Map[category][1][key];
+        } else if (router._week === 5) {
+            location = router.Map[category][2][key];
+        } else {
+            location = router.roll(...router.Map[category].map(item => item[key]));
+        }
+    } else {
+        location = router.roll(...router.Map[category].map(item => item[key]));
+    }
+    router.redirect(location);
+}
+
+doRedirect();
